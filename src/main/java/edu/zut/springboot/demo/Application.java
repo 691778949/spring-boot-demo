aa5dcc446656374a1638596fdf7c3704d6145c07
shijin <691778949@qq.com>
2019-01-01 17:36:28 +0800

    modify

--- src/main/java/edu/zut/springboot/demo/Application.java
+++ src/main/java/edu/zut/springboot/demo/Application.java
@@ -1,6 +1,5 @@
 package edu.zut.springboot.demo;
 
-import edu.zut.springboot.demo.config.JpaConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
