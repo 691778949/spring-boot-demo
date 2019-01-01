a529440c5cec73ad0f4c00b8818b80cd987372b8
shijin <691778949@qq.com>
2019-01-01 17:34:42 +0800

    modify

--- src/main/java/edu/zut/springboot/demo/controller/PersonController.java
+++ src/main/java/edu/zut/springboot/demo/controller/PersonController.java
@@ -1,7 +1,13 @@
 package edu.zut.springboot.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "HELLO";
+    }
 }
